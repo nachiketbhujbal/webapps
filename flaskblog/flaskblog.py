c374dce1ba23e33642c6c84d612c5e5078ef0293
nachiketbhujbal <nachiketbhujbal@outlook.com>
2020-04-01 01:16:03 -0400

    automated git push for /Users/nachiketbhujbal/sandbox/webapps/flaskblog

--- flaskblog/flaskblog.py
+++ flaskblog/flaskblog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationFrom, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bc643a510f94191f5e01e52938fbb866'
 
 posts = [
 
@@ -24,9 +26,24 @@ posts = [
 def home():
 	return render_template('home.html', posts=posts)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationFrom()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
