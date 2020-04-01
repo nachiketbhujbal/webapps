cfa1c7878dd9b4516a1f34db56ac4d41e44cf3ad
nachiketbhujbal <nachiketbhujbal@outlook.com>
2020-03-31 22:41:37 -0400

    adding layouts and test templates

--- flaskweb/flaskapp.py
+++ flaskweb/flaskapp.py
@@ -2,14 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+
+    {
+        'author': 'Nachiket Bhujbal',
+        'title': 'Blog Post 1',
+        'content': 'First post content body',
+        'date': 'March 31, 2020'
+        },
+    {
+        'author': 'Nachiket Bhujbal',
+        'title': 'HAPPY APRIL FOOLS',
+        'content': 'April fools day! Hahah',
+        'date': 'April 1, 2020'
+        }
+    ]
+
+
 @app.route('/')
 @app.route('/home')
 def home():
-	return render_template('home.html')
+	return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
