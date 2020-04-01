e17e3f658d799373c80910f0107e8a9cc6ae2fb7
nachiketbhujbal <nachiketbhujbal@outlook.com>
2020-04-01 01:23:33 -0400

    automated git push for /Users/nachiketbhujbal/sandbox/webapps/flaskblog

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -8,7 +8,8 @@ class RegistrationFrom(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(),
+                                                 EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
